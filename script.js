20b50b4e829aa6e0bb21adf484ebf89dbadc6b98
Eduardo Oliveira <emoliveira97@gmail.com>
2025-08-08 21:46:29 -0300

    Iniciando os eventos no arquivo script.js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-let nomeUsuario = "Eduardo";
-const CPF = "12345678910";
+// Eventos - 
+//Qual tipo? - click
+//E o que vai acontecer? - nova tarefa
+//Onde irá ocorrer? - button
+const listaU = document.querySelector("[data-ullist]");
+
+const novaTarefa = document.querySelector("[data-form-button]");
+
+
+// Evento
+
+const criarTarefa = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const valor = input.value;
+    console.log(valor);
+
+}
+
+novaTarefa.addEventListener('click', criarTarefa);
+
+// Espera um evento, e o que vai acontecer

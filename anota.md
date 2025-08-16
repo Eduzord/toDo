6086930406f4966f583bf70fc0cc311b21bdda35
Eduardo Oliveira <emoliveira97@gmail.com>
2025-08-15 21:44:24 -0300

    Adiciona elementos dentro do ul e criação do componente Botao Conclui

--- anota.md
+++ anota.md
@@ -8,3 +8,12 @@
 
 
 # Documentação do toDo
+`createElement()` -> Cria um novo elemento.
+`appendChild()` -> Insere uma tag filho no tag pai.
+`innerHTML` -> Atributo que insere um elemento dentro de outro elemento na mesma hierarquia.
+`classList` -> Atributo que acessa a lista de classes da tag.
+`add()` -> Adiciona alguma coisa.
+
+Preciso pegar a const conteúdo, inseri-la dentro de uma tag <li> e inserir essa tag <li> na <ul>.
+
+Preciso incluir um botão no item toda vez que eu criar um novo item.
--- script.js
+++ script.js
@@ -2,21 +2,74 @@
 //Qual tipo? - click
 //E o que vai acontecer? - nova tarefa
 //Onde irá ocorrer? - button
-const listaU = document.querySelector("[data-ullist]");
-
-const novaTarefa = document.querySelector("[data-form-button]");
 
 
-// Evento
+
+
+
+// let tarefa = ""
+
+
+
 
 const criarTarefa = (evento) => {
     evento.preventDefault();
+    const listaU = document.querySelector("[data-ullist]");
     const input = document.querySelector("[data-form-input]");
     const valor = input.value;
-    console.log(valor);
 
-}
+     if(valor === ""){
+            alert("Por favor, preencha o campo com uma tarefa.")
+            input.focus();
+            return;
+        };
 
-novaTarefa.addEventListener('click', criarTarefa);
+    const tarefa = document.createElement("li");
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+    tarefa.appendChild(BotaoConclui());
+    listaU.appendChild(tarefa);
+    
+    input.value = "";
+    input.focus();
+    
+    // Preciso pegar a const conteúdo, inseri-la dentro de uma tag <li> e inserir essa tag <li> na <ul>.
+    
+    // if(valor === ""){
+        //     alert("Por favor, preencha o campo com uma tarefa.")
+        //     input.focus();
+        //     return;
+        // };
+        
+        
+        // tarefa += `<li><p class = "task">${valor}</p></li>`
+        // listaU.innerHTML = tarefa;
+        
+        // input.value = "";
+        // input.focus();
+        
+        
+    }
+    // Evento
+    
+    // `createElement()` -> Cria um novo elemento
+    // `appendChild()` -> Insere uma tag filho no tag pai.
+    
+const btnNovaTarefa = document.querySelector("[data-form-button]");
+btnNovaTarefa.addEventListener('click', criarTarefa);
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'Concluir';
+
+    botaoConclui.addEventListener('click',() => {
+        console.log("Fui clicado");
+    });
+    return botaoConclui;
+
+};
 
 // Espera um evento, e o que vai acontecer
